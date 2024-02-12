0c55d9f68045be2299e4a5ef05207c5973e3217d
Jason Nunez <jason.nunez0@yahoo.com>
2024-02-12 16:27:45 -0500

    Create LoginPage Codebehind
    Create the codebehind for the login page

---
+++ code/java_project/src/edu/westga/cs3212/inventory_manager/view/LoginPage.java
@@ -0,0 +1,22 @@
+package edu.westga.cs3212.inventory_manager.view;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
+public class LoginPage {
+
+	@FXML
+    private Text companyTitleLabel;
+
+    @FXML
+    private TextField employeeIDTextField;
+
+    @FXML
+    private TextField passwordTextField;
+
+    @FXML
+    void attemptLogin(ActionEvent event) {
+
+    }
+}
