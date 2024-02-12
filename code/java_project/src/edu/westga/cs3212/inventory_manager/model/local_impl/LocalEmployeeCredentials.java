f5e7dc24a06aba55d2fb85ea81128f9e9f087838
Jason Nunez <jason.nunez0@yahoo.com>
2024-02-12 17:07:02 -0500

    Create LocalEmployeeCredentials
    The class that will hold the employee information

---
+++ code/java_project/src/edu/westga/cs3212/inventory_manager/model/local_impl/LocalEmployeeCredentials.java
@@ -0,0 +1,9 @@
+package edu.westga.cs3212.inventory_manager.model.local_impl;
+
+public class LocalEmployeeCredentials {
+	private String employeeID;
+	private String firstName;
+	private String lastName;
+	private String password;
+	private String employeeType; 
+}
