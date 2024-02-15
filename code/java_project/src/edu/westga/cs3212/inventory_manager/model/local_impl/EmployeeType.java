916c853a40e56d9a1f38cd9c6625949d50757a57
Jason Nunez <jason.nunez0@yahoo.com>
2024-02-15 10:26:20 -0500

    Add JavaDocs to the EmployeeType enum class
    Add Javadocs to the class headers

--- code/java_project/src/edu/westga/cs3212/inventory_manager/model/local_impl/EmployeeType.java
+++ code/java_project/src/edu/westga/cs3212/inventory_manager/model/local_impl/EmployeeType.java
@@ -1,5 +1,10 @@
 package edu.westga.cs3212.inventory_manager.model.local_impl;
 
+/**
+ * Represents the type of employee (e.g., MANAGER, WORKER).
+ *
+ * @author Jason Nunez
+ */
 public enum EmployeeType {
     MANAGER,
     WORKER
