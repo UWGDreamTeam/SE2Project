dca98c8c240444cb5e8d269169b9f07cc5706372
Jacob Evans <je00059@my.westga.edu>
2024-02-14 19:59:19 -0500

    Updated OrderManager documentation
    1) Updated docstrings for all methods in OrderManager.

--- code/java_project/src/edu/westga/cs3212/inventory_manager/model/OrderManager.java
+++ code/java_project/src/edu/westga/cs3212/inventory_manager/model/OrderManager.java
@@ -7,7 +7,10 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * The Order Manager Class.
+ * The Order Manager class manages a collection of orders.
+ * It provides methods to add, remove, and retrieve orders,
+ * as well as to filter orders based on completion status or creation date.
+ * Orders are stored in a map, where each order is associated with a unique ID.
  * 
  * @author Group 1
  * @version Spring 2024
@@ -17,16 +20,26 @@ public class OrderManager {
 	private Map<String, Order> orders;
 	
 	/**
-	 * Instantiates a new order manager.
+	 * Instantiates a new OrderManager with an empty collection of orders.
 	 */
 	public OrderManager() {
 		this.orders = new HashMap<String, Order>();
     }
 	
+	/**
+     * Retrieves a list of all orders in the order manager.
+     * 
+     * @return a list of all orders
+     */
 	public List<Order> getOrders() {
 		return this.orders.values().stream().toList();
 	}
 	
+	/**
+     * Retrieves a list of completed orders in the order manager.
+     * 
+     * @return a list of completed orders
+     */
 	public List<Order> getCompleteOrders() {
 		List<Order> completeOrders = new ArrayList<Order>();
 		for (Order order : this.orders.values()) {
@@ -37,6 +50,11 @@ public class OrderManager {
 		return completeOrders;
 	}
 	
+	/**
+     * Retrieves a list of incomplete orders in the order manager.
+     * 
+     * @return a list of incomplete orders
+     */
 	public List<Order> getIncompleteOrders() {
 		List<Order> incompleteOrders = new ArrayList<Order>();
 		for (Order order : this.orders.values()) {
@@ -47,6 +65,12 @@ public class OrderManager {
 		return incompleteOrders;
 	}
 	
+	/**
+     * Retrieves a list of orders created on a specified date.
+     * 
+     * @param date the date to filter orders by
+     * @return a list of orders created on the specified date
+     */
 	public List<Order> getOrdersByDate(LocalDateTime date) {
 		List<Order> orders = new ArrayList<Order>();
 		for (Order order : this.orders.values()) {
@@ -57,6 +81,11 @@ public class OrderManager {
 		return orders;
 	}
 	
+	/**
+     * Adds an order to the order manager.
+     * 
+     * @param order the order to add
+     */
 	public void addOrder(Order order) {
 		if (order == null) {
 			throw new IllegalArgumentException("Order cannot be null");
@@ -68,6 +97,11 @@ public class OrderManager {
 		}
 	}
 	
+	/**
+     * Removes an order from the order manager.
+     * 
+     * @param order the order to remove
+     */
 	public void removeOrder(Order order) {
         if (order == null) {
             throw new IllegalArgumentException("Order cannot be null");
@@ -79,6 +113,12 @@ public class OrderManager {
 		}
     }
 	
+	/**
+     * Finds an order by its ID.
+     * 
+     * @param id the ID of the order to find
+     * @return the order with the specified ID
+     */
 	public Order findOrderById(String id) {
 		if (id == null) {
             throw new IllegalArgumentException("Id cannot be null");
@@ -90,6 +130,11 @@ public class OrderManager {
 		}
     }
 	
+	/**
+     * Marks an order as completed.
+     * 
+     * @param order the order to mark as completed
+     */
 	public void completeOrder(Order order) {
 		if (order == null) {
             throw new IllegalArgumentException("Order cannot be null");
@@ -99,6 +144,11 @@ public class OrderManager {
 	    }
 	}
 	
+	/**
+     * Marks a completed order as incomplete.
+     * 
+     * @param order the completed order to mark as incomplete
+     */
 	public void undoOrderCompletion(Order order) {
 		if (order == null) {
             throw new IllegalArgumentException("Order cannot be null");
