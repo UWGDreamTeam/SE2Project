536fdacb70bbe6c7cdc8e4962d6384784b61bd56
Jason Nunez <jason.nunez0@yahoo.com>
2024-02-15 09:08:44 -0500

    Add the ability to get the employees from the LocalEmployeeCredentialsManager
    This will allow for the viewing of employees to be easier and allows for easier testing.

--- code/java_project/src/edu/westga/cs3212/inventory_manager/test/localemployeecredentialsmanager/TestConstructor.java
+++ code/java_project/src/edu/westga/cs3212/inventory_manager/test/localemployeecredentialsmanager/TestConstructor.java
@@ -2,13 +2,28 @@ package edu.westga.cs3212.inventory_manager.test.localemployeecredentialsmanager
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import edu.westga.cs3212.inventory_manager.model.local_impl.LocalEmployeeCredentialsManager;
 
 class TestConstructor {
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
+	private final Path credentialsFilePath = Paths.get("employeeCredentials.json");
+	
+	@BeforeEach
+	public void setUp() throws Exception {
+		Files.deleteIfExists(this.credentialsFilePath);
 	}
+
+	
+	@Test
+    public void testConstructor_initializesMap() {
+        LocalEmployeeCredentialsManager manager = new LocalEmployeeCredentialsManager();
+    }
 
 }
