78efc8a5265bccd366febd4e3b6f8606d9716470
Vitor dos Santos <vdossan1@my.westga.edu>
2024-02-24 23:28:23 -0500

    Add/Remove Features
    1) Successfully adds new component to inventory.
    2) Successfully remove item from inventory.
    3) DOES NOT automatically updates TableView after add/remove.

--- code/java_project/src/edu/westga/cs3212/inventory_manager/viewmodel/InventoryViewModel.java
+++ code/java_project/src/edu/westga/cs3212/inventory_manager/viewmodel/InventoryViewModel.java
@@ -1,7 +1,15 @@
 package edu.westga.cs3212.inventory_manager.viewmodel;
 
+import edu.westga.cs3212.inventory_manager.model.Item;
+import edu.westga.cs3212.inventory_manager.model.local_impl.Component;
 import edu.westga.cs3212.inventory_manager.model.local_impl.LocalComponentInventory;
 import edu.westga.cs3212.inventory_manager.model.local_impl.LocalProductInventory;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleListProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
 
 /**
  * The Class InventoryViewModel.
@@ -10,6 +18,9 @@ import edu.westga.cs3212.inventory_manager.model.local_impl.LocalProductInventor
  * @version Spring 2024
  */
 public class InventoryViewModel {
+	
+	private ObjectProperty<Item> selectedComponent;
+	private ObservableList<Item> listOfComponents;
 
 	private LocalComponentInventory componentsInventory;
 	private LocalProductInventory productInventory;
@@ -22,10 +33,17 @@ public class InventoryViewModel {
 	 * @postcondition none
 	 */
 	public InventoryViewModel() {
+		this.selectedComponent = new SimpleObjectProperty<Item>();
+		this.listOfComponents = new SimpleListProperty<Item>();
+		
 		this.componentsInventory = new LocalComponentInventory();
 		this.productInventory = new LocalProductInventory();
 	}
 	
+	public ObservableList<Item> getObservableComponentList(){
+		return FXCollections.observableArrayList(this.componentsInventory.getListOfItems());
+	}
+ 	
 	
 	/**
 	 * Adds the component.
@@ -33,4 +51,26 @@ public class InventoryViewModel {
 	public void addComponent() {
 		//TODO
 	}
+	
+	public void removeComponent() {
+		this.componentsInventory.removeItem(this.selectedComponent.getValue());
+	}
+
+	public ObjectProperty<Item> getSelectedComponent() {
+		return this.selectedComponent;
+	}
+
+	public ObservableList<Item> getListOfComponents() {
+		return this.listOfComponents;
+	}
+
+	public LocalComponentInventory getComponentsInventory() {
+		return this.componentsInventory;
+	}
+
+	public LocalProductInventory getProductInventory() {
+		return this.productInventory;
+	}
+	
+	
 }
