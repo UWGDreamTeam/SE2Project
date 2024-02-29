0506fe446e238e42fbec53c2a93da3430cc35074
Jason Nunez <jason.nunez0@yahoo.com>
2024-02-29 09:50:54 -0500

    Add the ability to go to the order page from home
    This will allow us to go from page to page

--- code/java_project/src/edu/westga/cs3212/inventory_manager/model/local_impl/LocalComponentInventory.java
+++ code/java_project/src/edu/westga/cs3212/inventory_manager/model/local_impl/LocalComponentInventory.java
@@ -42,14 +42,6 @@ public class LocalComponentInventory implements ItemInventoryManager {
 		}
 	}
 
-	private void createTestData() {
-		this.components = new HashMap<>();
-		this.components.put(new Component("1", 1.0), 10);
-		this.components.put(new Component("2", 2.0), 20);
-		this.components.put(new Component("3", 3.0), 30);
-		this.save();
-	}
-
 	private void save() {
 		ComponentInventoryStorage.save(this.components, Constants.COMPONENT_INVENTORY_FILE_LOCATION);
 	}
