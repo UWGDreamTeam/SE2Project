20d467bac28a1907e0a93a1d4a992accdad2b551
Jason Nunez <jason.nunez0@yahoo.com>
2024-02-29 07:02:45 -0500

    Override toString method of the component,
    This will help in writing the home summary outputs

--- code/java_project/src/edu/westga/cs3212/inventory_manager/viewmodel/HomePageViewModel.java
+++ code/java_project/src/edu/westga/cs3212/inventory_manager/viewmodel/HomePageViewModel.java
@@ -2,45 +2,48 @@ package edu.westga.cs3212.inventory_manager.viewmodel;
 
 import java.util.ArrayList;
 
+import edu.westga.cs3212.inventory_manager.model.ComponentInventoryAnalytics;
 import edu.westga.cs3212.inventory_manager.model.Item;
 import edu.westga.cs3212.inventory_manager.model.Product;
 import edu.westga.cs3212.inventory_manager.model.local_impl.LocalComponentInventory;
 import edu.westga.cs3212.inventory_manager.model.local_impl.LocalProductInventory;
 import javafx.beans.property.SimpleListProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 import javafx.scene.chart.XYChart;
 
 public class HomePageViewModel {
 	
-	LocalComponentInventory componentManager;
-	LocalProductInventory productManager;
-	//TODO add property for orders
+	private StringProperty componentSumarryTextArea;
+	private StringProperty productSumarryTextArea;
+	private StringProperty orderSumarryTextArea;
+	private ComponentInventoryAnalytics componentInventoryAnalytics;
 	
-	private SimpleListProperty<XYChart.Series<String, Number>> commonComponentsBarChart;
-	private SimpleListProperty<XYChart.Series<String, Number>> commonProductBarChart;
-	private SimpleListProperty<XYChart.Series<String, Number>> ordersBarChart;
+	
 	
 	public HomePageViewModel() {
-		this.componentManager = new LocalComponentInventory();
-		this.productManager = new LocalProductInventory();
-		this.commonComponentsBarChart = new SimpleListProperty<XYChart.Series<String, Number>>();
-		this.commonProductBarChart = new SimpleListProperty<XYChart.Series<String, Number>>();
-		this.ordersBarChart = new SimpleListProperty<XYChart.Series<String, Number>>();
-	}
-
-	public SimpleListProperty<XYChart.Series<String, Number>> getCommonComponentsBarChart() {
-		return commonComponentsBarChart;
-	}
-
-	public SimpleListProperty<XYChart.Series<String, Number>> getCommonProductBarChart() {
-		return commonProductBarChart;
-	}
-
-	public SimpleListProperty<XYChart.Series<String, Number>> getOrdersBarChart() {
-		return ordersBarChart;
+		this.componentSumarryTextArea = new SimpleStringProperty();
+		this.productSumarryTextArea = new SimpleStringProperty();
+		this.orderSumarryTextArea = new SimpleStringProperty();
 	}
 	
-	private void test() {
-		Iterable<Product> products = this.productManager.getProducts();
+	
+	public StringProperty getComponentSumarryTextArea() {
+		return componentSumarryTextArea;
+	}
+
+	public StringProperty getProductSumarryTextArea() {
+		return productSumarryTextArea;
+	}
+
+	public StringProperty getOrderSumarryTextArea() {
+		return orderSumarryTextArea;
+	}
+
+
+
+	public void getComponetSumarry() {
+		Compon
 	}
 	
 	
