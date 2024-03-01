590c05b46cc55d9d85065a9f7aabd659f660d1d7
Jason Nunez <55280347+JasonNunez@users.noreply.github.com>
2024-03-01 01:30:46 -0500

    Add Test Cases for the private constructor of the employees credential storage
    This will ensure that the utility class can not be created

---
+++ code/java_project/src/edu/westga/cs3212/inventory_manager/test/employeecredentialsstorage/TestSave.java
@@ -0,0 +1,54 @@
+package edu.westga.cs3212.inventory_manager.test.employeecredentialsstorage;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Modifier;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.jupiter.api.Test;
+
+import edu.westga.cs3212.inventory_manager.model.EmployeeCredentialsStorage;
+import edu.westga.cs3212.inventory_manager.model.local_impl.LocalEmployeeCredentials;
+
+public class TestSave {
+
+	@Test
+    public void testConstructorIsPrivate() throws NoSuchMethodException, IllegalAccessException {
+        Constructor<EmployeeCredentialsStorage> constructor = EmployeeCredentialsStorage.class.getDeclaredConstructor();
+        assertTrue(Modifier.isPrivate(constructor.getModifiers()), "Constructor is not private");
+        
+        constructor.setAccessible(true); // Make the private constructor accessible
+        
+        try {
+            constructor.newInstance();
+            fail("Constructor invocation should throw an IllegalStateException");
+        } catch (InvocationTargetException e) {
+            assertTrue(e.getCause() instanceof IllegalStateException, "Cause of InvocationTargetException should be IllegalStateException");
+            assertEquals(EmployeeCredentialsStorage.UTILITY_CLASS_ERROR, e.getCause().getMessage(), "Exception message does not match");
+        } catch (InstantiationException e) {
+            fail("Instantiation exception should not occur");
+        }
+    }
+	
+    @Test
+    void testSaveWithInvalidFilePathThrowsException() {
+    	Map<String, LocalEmployeeCredentials> employeeCredentialsMap = new HashMap<>();
+
+        String invalidFilePath = "/invalid/path/to/file";
+		assertEquals(0, employeeCredentialsMap.size());
+		EmployeeCredentialsStorage.save(employeeCredentialsMap, invalidFilePath);
+    }
+
+    @Test
+    void testSaveWithFilePathAsDirectoryThrowsException() {
+    	Map<String, LocalEmployeeCredentials> employeeCredentialsMap = new HashMap<>();
+        String filePathAsDirectory = "/tmp"; 
+        EmployeeCredentialsStorage.save(employeeCredentialsMap, filePathAsDirectory);
+        assertEquals(0, employeeCredentialsMap.size());
+    }
+}
