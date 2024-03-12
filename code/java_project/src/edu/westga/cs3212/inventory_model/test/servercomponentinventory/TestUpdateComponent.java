2107f141cce0e9281788ed94d3fe20843abb80c2
Jason Nunez <jason.nunez0@yahoo.com>
2024-03-12 09:52:43 -0400

    Add test cases for updating a component
    This will ensure that the edge cases of updating a component are covered

---
+++ code/java_project/src/edu/westga/cs3212/inventory_model/test/servercomponentinventory/TestUpdateComponent.java
@@ -0,0 +1,59 @@
+package edu.westga.cs3212.inventory_model.test.servercomponentinventory;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import edu.westga.cs3212.inventory_manager.model.server_impl.ComponentInventory;
+
+class TestUpdateComponent {
+
+	@Test
+	void testUpdateComponentWithNullID() {
+		assertThrows(IllegalArgumentException.class, () -> {
+			ComponentInventory.updateComponent(null, "NewName", 0.0, 0);
+		});
+	}
+	
+	@Test
+	void testUpdateComponentWithBlankID() {
+		assertThrows(IllegalArgumentException.class, () -> {
+			ComponentInventory.updateComponent("", "NewName", 0.0, 0);
+		});
+	}
+	
+	@Test
+	void testUpdateComponentWithNullName() {
+		assertThrows(IllegalArgumentException.class, () -> {
+			ComponentInventory.updateComponent("ID", null, 0.0, 0);
+		});
+	}
+	
+	@Test
+	void testUpdateComponentWithBlankName() {
+		assertThrows(IllegalArgumentException.class, () -> {
+			ComponentInventory.updateComponent("ID", "", 0.0, 0);
+		});
+	}
+	
+	@Test
+	void testUpdateComponentWithNegativeProductionCost() {
+		assertThrows(IllegalArgumentException.class, () -> {
+			ComponentInventory.updateComponent("ID", "NewName", -1.0, 0);
+		});
+	}
+	
+	@Test
+	void testUpdateComponentWhenIDNotFound() {
+		assertThrows(IllegalArgumentException.class, () -> {
+			ComponentInventory.updateComponent("ID", "NewName", 0.0, 0);
+		});
+	}
+	
+	@Test
+	void testUpdateComponentWithValidData() {
+		String ComponentID = ComponentInventory.addComponent("Name", 0.0, 0);
+		ComponentInventory.updateComponent(ComponentID, "NewName", 0.0, 0);
+	}
+
+}
