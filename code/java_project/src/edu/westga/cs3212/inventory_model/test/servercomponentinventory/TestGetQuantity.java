55ac1c13daf09d0400861a1c5a74d5b8e55a07e0
Jason Nunez <jason.nunez0@yahoo.com>
2024-03-12 10:12:42 -0400

    Create test cases for getting quantity of a component

---
+++ code/java_project/src/edu/westga/cs3212/inventory_model/test/servercomponentinventory/TestGetQuantity.java
@@ -0,0 +1,38 @@
+package edu.westga.cs3212.inventory_model.test.servercomponentinventory;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import edu.westga.cs3212.inventory_manager.model.server_impl.ComponentInventory;
+
+class TestGetQuantity {
+
+	@Test
+	void testGetQuantityWithNullID() {
+		assertThrows(IllegalArgumentException.class, () -> {
+			ComponentInventory.getQuantity(null);
+		});
+	}
+	
+	@Test
+	void testGetQuantityWithBlankID() {
+		assertThrows(IllegalArgumentException.class, () -> {
+			ComponentInventory.getQuantity("");
+		});
+	}
+	
+	@Test
+	void testGetQuantityWithComponentNotInInventory() {
+		assertThrows(IllegalArgumentException.class, () -> {
+			ComponentInventory.getQuantity("123");
+		});
+	}
+	
+	@Test
+	void testGetQuantityWithComponentInInventory() {
+		String response = ComponentInventory.addComponent("TestComponent", 1.0, 10);
+		assertEquals(10, ComponentInventory.getQuantity(response));
+	}
+
+}
